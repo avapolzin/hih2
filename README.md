b9d32610ec15ef8519091004a77262895bdac270
avapolzin <apolzin@uchicago.edu>
2026-04-18 17:15:05 -0500

    summary table, start of vol.py

--- README.md
+++ README.md
@@ -23,16 +23,37 @@ Included models are:
 - [Krumholz (2013)](https://ui.adsabs.harvard.edu/abs/2013MNRAS.436.2747K/abstract) -- K13
 - [Sternberg et al. (2014)](https://ui.adsabs.harvard.edu/abs/2014ApJ...790...10S/abstract) -- S14
 - [Gnedin & Draine (2014)](https://ui.adsabs.harvard.edu/abs/2014ApJ...795...37G/abstract) -- GD14
-- [Polzin et al. (2024)](https://ui.adsabs.harvard.edu/abs/2024ApJ...966..172P/abstract) -- P24
+- [Polzin et al. (2024a)](https://ui.adsabs.harvard.edu/abs/2024ApJ...966..172P/abstract) -- P24
 
 Volumetric models are in `hih2.vol` and projected models are in `hih2.proj`. All syntax is the same between those modules. It's very simple, so the docstrings are largely sufficient and limited documentation follows here:
 
-All models take as arguments `nh` (hydrogen number density if `hih2.vol` -- cm<sup>-3</sup> by default -- or hydrogen column density if `hih2.proj` -- cm<sup>-2</sup> by default), `met` (solar-scaled metallicity), and `uv` (UV field strength). All of the functions in `hih2.proj` also require an `scale` argument, is the size of the grid cells (by default in cm). 
+All models take as arguments `nh` (hydrogen number density if `hih2.vol` -- cm<sup>-3</sup> by default -- or hydrogen column density if `hih2.proj` -- cm<sup>-2</sup> by default) and `met` (solar-scaled metallicity). `gk11()` and `p24()` require `uv` (UV field strength). All of the functions in `hih2.proj` also require an `scale` argument, is the size of the grid cells (by default in cm). `hih2.vol.gd14()`, `kmt09b`, `k13`, and `s14` also take a `scale` argument, which follows the convention of `hih2.proj`.
 
-Both `k13()` and `s14()` functions also take a clumping factor argument, `fc`, and `k13()` additionally takes `rho_sd` (density of stars and dark matter, by default in M<sub>&#9737;</sub> pc<sup>-3</sup>), `iterate` (toggles whether values are computed iteratively), and `niter` (if `iterate = True`, sets number of interations).
+Both `k13()` and `s14()` functions also take a clumping factor argument, `fc`, and `k13()` additionally takes `rho_sd` (density of stars and dark matter, by default in M<sub>&#9737;</sub> pc<sup>-3</sup>), `iterate` (toggles whether values are computed iteratively), and `niter` (if `iterate = True`, sets number of interations). 
 
-All functions take `dens_unit`, which takes `astropy.units` objects to set the units in which `nh` is reported. `k13()` has an equivalent for `rho_sd` called `sddens_unit`. 
+All functions take `dens_unit`, which takes `astropy.units` objects to set the units in which `nh` is reported. `k13()` has an equivalent for `rho_sd` called `sddens_unit`. `hih2.proj` and `hih2.vol.gd14()` take a `scale_unit` argument.
 
+Each function returns the molecular hydrogen fraction (defined as H<sub>2</sub>/(HI + H<sub>2</sub>)) for the input gas density, metallicity, and UV field strength. The implicit assumption of these models is that all gas is neutral and that any ionized component is negligible.
 
 **Why is this a package?** Good question. It's very easy to implement these HI-H<sub>2</sub> models yourself either from the literature or from code examples (see [avapolzin/hydrogen_models](https://github.com/avapolzin/hydrogen_models) for functions on their own), but some people may prefer a quick install/import. 
+
+**Summary of required and optional parameters in models included in `hih2.vol`**
+| Model | n<sub>H</sub> | metallicity | UV field | scale | f<sub>c</sub> | &rho;<sub>SD</sub> |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| `kmt09b` | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  |  $${\color{red}Required}$$| | | 
+| `gk11` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  | | | 
+| `k13` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{yellow}Required, default provided}$$ | $${\color{yellow}Required, default provided}$$| 
+| `s14` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{yellow}Required, default provided}$$| | 
+| `gd14` | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  | $${\color{red}Required}$$ | | | 
+| `p24` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  | | | 
+
+**Summary of required and optional parameters in models included in `hih2.proj`**
+| Model | N<sub>H</sub> | metallicity | UV field | scale | f<sub>c</sub> | &rho;<sub>SD</sub> |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: |
+| `kmt09b` | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  |  $${\color{red}Required}$$| | | 
+| `gk11` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  | $${\color{red}Required}$$ | | 
+| `k13` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{yellow}Required, default provided}$$ | $${\color{yellow}Required, default provided}$$| 
+| `s14` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{yellow}Required, default provided}$$| | 
+| `gd14` | $${\color{red}Required}$$ | $${\color{red}Required}$$ |  | $${\color{red}Required}$$ | | | 
+| `p24` | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | $${\color{red}Required}$$ | | | 
 
---
+++ hih2/vol/vol.py
@@ -0,0 +1,209 @@
+import numpy as np
+import astropy.units as u
+import astropy.constants as c
+
+# #########
+#  * * * * 
+# #########
+
+def p24(nh, met, uv, dens_unit = u.cm**-3):
+	"""
+	Return volumetric molecular fraction from Polzin et al. (2024a)
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3 by default)
+		met (arr-like): metallicity in solar units
+		uv (arr-like): normalized UV field strength
+		dens_unit (astropy units object): units attached to input nh
+	"""
+
+	nh *= dens_unit.to(u.cm**-3).value # convert units if necessary
+
+	d = 0.0199*met
+	a = -34.7*uv**0.32 + 2.25*met**0.3
+	b = -53.9*uv**0.31
+	c = met/0.2
+	ntr= a*np.log10(d) + b + c
+	ntr[ntr < 0.1] = 0.1
+
+	x = np.log(nh/ntr)
+
+	r0 = 3.5e-17
+	q = 30*u.Myr.to(u.s) * r0 * 0.2*(met/0.2)**1.3 * nh
+	maxf = 1 - np.exp(-q)
+	fm = 1/(1 + 2*(1 - maxf)/maxf)
+
+	fmol = fm/(1 + np.exp(-(7.6*(met)**0.25)*x + np.log(fm/6e-4)))
+
+	return fmol
+
+
+def gk11(nh, met, uv, dens_unit = u.cm**-3):
+	"""
+	Return molecular fraction from Gnedin & Kravtsov (2011)
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3 by default)
+		met (arr-like): metallicity in solar units
+		uv (arr-like): normalized UV field strength
+		dens_unit (astropy units object): units attached to input nh
+	"""
+	nh *= dens_unit.to(u.cm**-3).value # convert units if necessary
+
+	dmw = met
+
+	nstar = 25 #cm^-3
+	dstar = 1.5e-3*np.log(1 + (3*uv)**(1.7))
+	alpha = 5*(uv/2)/(1 + (uv/2)**2)
+	s = 0.04/(dstar + dmw)
+	g = (1 + alpha*s + s**2)/(1 + s)
+	lam = np.log(1 + g*dmw**(3/7)*(u/15)**(4/7))
+	x = lam**(3/7) * np.log(dmw * nh/(lam*nstar))
+
+	fmol = 1/(1 + np.exp(-4*x - 3*x**3))
+	
+	return fmol
+
+
+def gd14(nh, met, scale, uv = None, dens_unit = u.cm**-3, scale_unit = u.cm):
+	"""
+	Return molecular fraction from Gnedin & Draine (2014)
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3 by default)
+		met (arr-like): metallicity in solar units
+		scale (arr-like): scale of grid cell in cm
+		uv (arr-like): normalized UV field strength
+		dens_unit (astropy units object): units attached to input nh
+		scale_unit (astropy units object): units attached to input scale
+	"""
+	nh *= dens_unit.to(u.cm**-3).value # convert units if necessary
+	scale *= scale_unit.to(u.cm).value
+
+
+	s = scale*u.cm.to(u.pc)/100
+	dmw = met
+
+	dstar = 0.17*(2 + s**5)/(1 + s**5)
+	ustar = 9*dstar/s
+	nstar = 14 * dstar**(1/2)/s #cm^-3
+	g = (dmw**2 + dstar**2)**(1/2)
+	lam = np.log(1 + (0.05/g + u)**(2/3) * g**(1/3)/ustar)
+	nhalf = nstar*lam/g
+	w = 0.8 + lam**(1/2)/s**(1/3)
+	x = w*np.log(nh/nhalf)
+
+	fmol = 1/(1 + np.exp(-x*(1 - 0.02*x + 0.001*x**2)))
+
+	return fmol
+
+def kmt09b(nh, z, l, uv = None, dens_unit = u.cm**-3, scale_unit = u.cm):
+	"""
+	Return molecular fraction from Krumholz et al. (2009b)
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3)
+		z (arr-like): metallicity in solar units
+		u (arr-like): normalized UV field strength
+		l (arr-like): scale of grid cell in cm
+	"""
+
+	sig_comp = nh*l*c.m_p.to(uu.g).value #should be on 100 pc scales, but will see if this works
+
+	chi = 0.77*(1 + 3.1*z**0.365)
+	sig0 = sig_comp/(1*(uu.Msun/uu.pc**2).to(uu.g/uu.cm**2))
+	s = np.log(1 + 0.6*chi)/(0.04*sig0 * z)
+	delta = 0.0712*(0.1/s + 0.675)**(-2.8)
+
+	fmol = 1 - (1 + ((3/4) * (s/(1 + delta)))**(-5))**(-1/5)
+
+	return fmol
+
+def k13(nh, z, uv, l, rho_sd = 1e-2, fc = 1, iter_ = True, niter = 10):
+	"""
+	Return molecular fraction from Krumholz (2013)
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3)
+		z (arr-like): metallicity in solar units
+		u (arr-like): normalized UV field strength
+		l (arr-like): scale of grid cell in cm
+		fc (int) : ~Unity on scales <~100 pc. Scales ~1 kpc -> fc = 5.
+		iter (bool): If iter, iterate niter times to compute fmol.
+		niter (int): Number of iterations since fmol necessary to compute fmol.
+	"""
+	a = 5
+	cw = 8*u.km/u.s
+	fw = 0.5
+	zeta_d = 0.33
+	if type(rho_sd) == np.ndarray:
+		## accounting for input from simulation directly
+		rho_sd = rho_sd*u.Msun/(u.pc**3)
+		# print(rho_sd)
+	if type(rho_sd) != np.ndarray:
+		#set single value between 1e-5 and 1e-1 Msun/pc**3 -- varies with galaxy structure, will use constant for now
+		rho_sd = rho_sd#*uu.Msun/uu.pc**3 #rho_sd
+	G = c.G.to(u.pc/u.Msun*(u.km/u.s)**2)
+	## since generally low fH2 regime will take Sigma_HI ~ Sigma_H (see K13, Sec. 2.2)
+	sig0 = (nh*l*c.m_p.to(u.g).value)*(u.g/u.cm**2).to(u.Msun/u.pc**2)*u.Msun/u.pc**2
+	ncnm_2p = 23*u*(4.1/(1 + 3.1*z**0.365)) #cm-3
+	if not iter_:
+		ncnm_hydro = (np.sqrt(2*np.pi*G*zeta_d*fw*rho_sd/a) * (cw/(1.1*c.k_B*243*u.K))*sig0).to(u.cm**-3).value
+
+		ncnm = np.maximum(ncnm_2p, ncnm_hydro)
+		chi = 7.2*uv*(ncnm/10)**(-1)
+
+		tau_c = 0.066*fc*z*sig0.value
+		s = np.log(1 + 0.6*chi + 0.01*chi**2)/(0.6*tau_c)
+
+		fmol = 1 - (3/4)*s/(1 + 0.25 * s)
+		fmol[s >= 2] = 0
+
+	if iter_:
+		fmol = np.full_like(nh, 0.5)
+		for i in range(niter):
+			## for right now, not worrying about an error threshold -- ~following Diemer+18
+			rh2 = fmol/(1 - fmol)
+			sig_hi = (1 - fmol)*sig0 #assuming all neutral hydrogen
+			pth = ((np.pi*G*sig_hi**2/(4*a))*
+				  (1 + 2*rh2 + np.sqrt((1 + 2*rh2)**2 + 
+						((32*zeta_d*a*fw*cw**2*rho_sd)/(np.pi*G*sig_hi**2)))))
+			ncnm_hydro = (pth/(1.1*c.k_B*243*u.K)).to(u.cm**-3).value
+
+			ncnm = np.maximum(ncnm_2p, ncnm_hydro)
+			chi = 7.2*uv*(ncnm/10)**(-1)
+
+			tau_c = 0.066*fc*z*sig0.value
+			s = np.log(1 + 0.6*chi + 0.01*chi**2)/(0.6*tau_c)
+
+			new_fmol = 1 - (3/4)*s/(1 + 0.25 * s)
+			new_fmol[s >= 2] = 0
+
+			fmol = 0.3*fmol + 0.7*new_fmol
+
+	return fmol
+
+def s14(nh, z, uv, l, fc = 1):
+	"""
+	Return molecular fraction from Sternberg et al. (2014)
+
+	Following Diemer et al. (2018), Section C.5
+
+	Parameters: 
+		nh (arr-like): neutral hydrogen number density (cm^-3)
+		z (arr-like): metallicity in solar units
+		u (arr-like): normalized UV field strength
+		l (arr-like): scale of grid cell in cm
+		fc (int): clumping factor
+	"""
+	g = 3e-5 * z * (9.9/(1 + 8.9*z))
+	d0 = 5.8e-11*uv #s-1
+	r = 3e-17 #cm-3 s-1
+	ag = (d0*g)/(r*nh)
+	nhi = 5.3e20 * ((1/z) * np.log((ag/fc)/2 + 1)) #cm-2
+	nh_ = nh*l
+
+	fmol = 1 - nhi/nh_
+	fmol[nhi > nh_] = 0
+
+	return fmol
